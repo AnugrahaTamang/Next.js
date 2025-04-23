a317aafa4be3ee187d315954fa18682a68e1be6b
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-04-23 15:13:24 +0545

    routing

---
+++ 1.Basic-Router/my-app/src/app/project/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Project = () => {
+  return (
+    <div>
+      <h1>This is all about project</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo, deserunt
+        est quasi iusto ipsum perspiciatis dolor dolore deleniti eum delectus
+        similique dolores exercitationem quod placeat suscipit blanditiis
+        quaerat consequatur magni optio aut. Reprehenderit ipsam est error.
+        Possimus, optio magni blanditiis adipisci dolores aliquid quod, et ad
+        quasi, sed beatae. Perferendis corrupti nobis sequi vel beatae eius
+        obcaecati sapiente ea fugiat neque facilis possimus, blanditiis debitis
+        reiciendis mollitia dolorum quas ducimus cumque, amet vero ad! Provident
+        sit unde dolore nemo aut corrupti minima ea! Odio natus minus laudantium
+        veritatis autem neque ullam, doloremque recusandae quidem veniam
+        explicabo, nulla vel sequi nam?
+      </p>
+    </div>
+  );
+};
+
+export default Project;
